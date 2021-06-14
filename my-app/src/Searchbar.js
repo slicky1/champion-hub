7d0f71ace578a4c8e95399c70487fac7355a8b41
slicky1 <okunonke@gmail.com>
2021-06-13 23:55:40 -0400

    database update

---
+++ my-app/src/Searchbar.js
@@ -0,0 +1,17 @@
+const Searchbar = ({placeholder, handleChanges}) => {
+    function handleChange (e){
+        handleChanges(e.target.value);
+    }
+    
+    return ( 
+        <div className= 'search'>
+            
+            <input type ='search'
+            placeholder = {placeholder}
+             onChange = {handleChange} //Has to be a function
+            />
+        </div>
+     );
+}
+ 
+export default Searchbar;
